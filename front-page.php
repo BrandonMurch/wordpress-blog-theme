b0a897b2199e36db7b49d98e938e9321be1986f7
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2021-12-27 10:19:06 +0200

    Add posts to main page with pagination

--- front-page.php
+++ front-page.php
@@ -1,5 +1,35 @@
 <?php get_header(); ?>
 
+<main class="container">
+
+  <?php if (have_posts()):
+      while (have_posts()):
+          the_post(); ?>
+          <div class="card">
+            <h1>
+              <?php the_title(); ?>
+            </h1>
+            <p>
+              <?php the_excerpt(); ?>
+            </p>
+            <a href="<?php echo get_permalink(); ?>">Read More</a>
+
+          </div>
+
+  <?php
+      endwhile; ?>
+
+      <div class="nav-previous alignleft">
+        <?php next_posts_link("Older posts"); ?>
+      </div>
+
+      <div class="nav-next alignright">
+        <?php previous_posts_link("Newer posts"); ?>
+      </div>
+  <?php
+  endif; ?>
+
+</main>
 
 
 <?php get_footer(); ?>
