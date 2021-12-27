a31038ee4c2f2d04a7324c7bfb58b9dfa9e54169
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2021-12-27 14:30:40 +0200

    Add logo

--- header.php
+++ header.php
@@ -4,12 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo( 'name' ); wp_title(); ?></title>
-    <meta name="description" content="<?php bloginfo( 'description' ) ?>">
-    <?php wp_head() ?>
+    <title><?php
+    bloginfo("name");
+    wp_title();
+    ?></title>
+    <meta name="description" content="<?php bloginfo("description"); ?>">
+    <?php wp_head(); ?>
 </head>
 <body>
 
-<header>
-    <div class="container header"></div>
+<header class="header">
+    <div class="logo-container">
+      <a class="logo" href="<?php echo site_url(); ?>">
+        bm
+      </a>
+
+
+    </div>
 </header>
