106a7213c17f4839dabc24d93a319172d9c06233
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2021-12-31 07:25:08 +0200

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
-var path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var path = require('path');
 
 // change these variables to fit your project
-const jsPath = "./js";
-const cssPath = "./sass";
-const outputPath = "dist";
-const localDomain = "localhost";
+const jsPath = './js';
+const cssPath = './sass';
+const outputPath = 'dist';
+const localDomain = 'localhost';
 const entryPoints = {
   // 'app' is the output name, people commonly use 'bundle'
   // you can have more than 1 entry point
-  app: jsPath + "/app.js",
-  style: cssPath + "/style.sass",
+  app: jsPath + '/app.js',
+  style: cssPath + '/style.sass',
 };
 
 module.exports = {
   entry: entryPoints,
   output: {
     path: path.resolve(__dirname, outputPath),
-    filename: "[name].js",
+    filename: '[name].js',
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: '[name].css',
     }),
 
     // Uncomment this if you want to use CSS Live reload
     new BrowserSyncPlugin(
       {
         proxy: localDomain,
-        files: [outputPath + "/*.css"],
+        files: [outputPath + '/*.css'],
         injectCss: true,
       },
       { reload: false }
@@ -39,15 +39,15 @@ module.exports = {
     rules: [
       {
         test: /\.s?[c]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.sass$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
+          'css-loader',
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               sassOptions: { indentedSyntax: true },
             },
@@ -56,7 +56,7 @@ module.exports = {
       },
       {
         test: /\.(jpg|jpeg|png|gif|woff|woff2|eot|ttf|svg)$/i,
-        use: "url-loader?limit=1024",
+        use: 'url-loader?limit=1024',
       },
     ],
   },
