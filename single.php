ea1f67ab0cd49af9566e11d94232d3c8faf4cc58
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2021-12-31 06:49:08 +0200

    Add post tags and cleanup

--- single.php
+++ single.php
@@ -4,8 +4,10 @@
   <h1 class="display-3"><?php the_title(); ?></h1>
   <p><?php the_content(); ?></p>
 
+  <p><?php wp_get_post_tags(); ?></p>
+
   <?php if (comments_open() || get_comments_number()):
-      comments_template($file = "/comments.php", $separate_comments = false);
+      comments_template();
   endif; ?>
 
 </main>
