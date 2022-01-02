1f6043cf8915121a8b1ef0f7ad33abccf7125f64
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2022-01-02 09:59:33 -0500

    Fix card appearances

--- post-list-card.php
+++ post-list-card.php
@@ -2,34 +2,41 @@
   <?php $has_been_modifed =
       get_the_time(get_option("date_format")) != get_the_modified_date(); ?>
 
-  <a href="<?php the_permalink(); ?>" class="card-body">
-      <?php if (has_post_thumbnail()) {
-          the_post_thumbnail("thumbnail");
-      } ?>
-      <h1 class="card-title">
-      <?php the_title(); ?>
-      </h1>
-      <div class="row">
-        <div class="col text-center">
-          <p class="text-muted">Written by <br> <strong> <?php the_author(); ?> </strong></p>
-          <p>
-        </div>
+      <!-- Div tag so the image can float -->
+      <div>
+        <?php if (has_post_thumbnail()) {
+            the_post_thumbnail("thumbnail");
+        } ?>
+        <h1 class="card-title">
+        <?php the_title(); ?>
+        </h1>
+        <div class="row">
+          <div class="col text-center">
+            <p class="text-muted">Written by <br> <strong> <?php the_author(); ?> </strong></p>
+            <p>
+          </div>
 
-        <div class="col text-center">
-          <p class="text-muted"> <?php echo $has_been_modifed
-              ? "Last updated"
-              : "Created on"; ?> <br> <strong><?php the_modified_date(); ?></strong>
-          </p>
-        </div>
+          <div class="col text-center">
+            <p class="text-muted"> <?php echo $has_been_modifed
+                ? "Last updated"
+                : "Created on"; ?> <br> <strong><?php the_modified_date(); ?></strong>
+            </p>
+          </div>
 
+        </div>
       </div>
 
 
-        <?php the_excerpt(); ?>
+      <?php the_excerpt(); ?>
       </p>
-      <div class="text-center">
-        <h2 class="card-action">Click to read more</h2>
-      </div>
-  </a>
+
+
+
+      <!-- allow link to span the entire card, only display text within the link for accessibilty readers -->
+      <h2 class="card-action" aria-hidden>Click to read more</h2>
+      <a href="<?php the_permalink(); ?>" class="card-link-overlay">
+        <p class="hidden">Click to read more</p>
+      </a>
+
 
 </article>
