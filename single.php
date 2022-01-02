9dbff01b424ab5b4007ff0ddf9e69ade98d46424
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2022-01-02 09:40:09 -0500

    Only display updated date if post has been updated

--- single.php
+++ single.php
@@ -6,6 +6,9 @@ $author_name =
     get_the_author_meta("first_name", $author_id) .
     " " .
     get_the_author_meta("last_name", $author_id);
+
+$has_been_modifed =
+    get_the_time(get_option("date_format")) != get_the_modified_date();
 ?>
 
 <main class="container">
@@ -16,8 +19,10 @@ $author_name =
 
   <h1 class="display-3 article-title"><?php the_title(); ?></h1>
 
-  <div class="row article-meta-container">
-    <div class="col article-meta">
+  <div class="article-meta-container">
+    <div class="<?php echo $has_been_modifed
+        ? "article-author"
+        : "article-meta-single"; ?>">
       <?php if (get_the_author_meta("custom-avatar", $author_id)): ?>
         <img class="article-avatar" src="<?php echo esc_url(
             get_the_author_meta("custom-avatar", $author_id)
@@ -37,8 +42,18 @@ $author_name =
       </p>
     </div>
 
-    <div class="col article-meta">
-      <p class="text-muted">Last updated <br> <strong><?php the_modified_date(); ?></strong>
+    <div class="<?php echo $has_been_modifed
+        ? "article-date"
+        : "article-meta-single"; ?>">
+      <p class="text-muted">Created on <br> <strong><?php echo the_time(
+          get_option("date_format")
+      ); ?></strong>
+
+
+
+      <?php if ($has_been_modifed): ?>
+        <p class="text-muted">Last updated <br> <strong><?php echo get_the_modified_date(); ?></strong>
+      <?php endif; ?>
       </p>
     </div>
 
