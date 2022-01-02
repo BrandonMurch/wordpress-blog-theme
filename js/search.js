3b78852ef7e653c247b75dad58fab86607d0a4bf
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2022-01-02 08:42:52 -0500

    Set timeout on focussing input
    Cleanup

--- js/search.js
+++ js/search.js
@@ -1,15 +1,18 @@
 const closeSearch = () => {
-    jQuery('#searchform')[0].reset();
-    jQuery('.searchform').removeClass('search-open');
-  };
+  jQuery("#searchform")[0].reset();
+  jQuery(".searchform").removeClass("search-open");
+};
 
 const openSearch = () => {
-    jQuery('.searchform').addClass('search-open');
-    jQuery('#s').focus();
-  };
+  jQuery(".searchform").addClass("search-open");
+  // Doesnt' look write if cursor appears immediately. It appears directly behind search icon. Wait until input has partially opened.
+  setTimeout(() => {
+    jQuery("#s").focus();
+  }, 200);
+};
 
 const stopSearchIfEmpty = (e) => {
-  if (jQuery('#s').val() == '') {
+  if (jQuery("#s").val() == "") {
     e.preventDefault();
     return false;
   }
@@ -17,13 +20,13 @@ const stopSearchIfEmpty = (e) => {
 
 export const searchBarHandler = () => {
   jQuery(window).load(() => {
-      jQuery('.search-close').on('click', closeSearch);
-      jQuery('.search-toggle').on('click', openSearch);
-      jQuery('.searchsubmit-container').on('click', stopSearchIfEmpty);
+    jQuery(".search-close").on("click", closeSearch);
+    jQuery(".search-toggle").on("click", openSearch);
+    jQuery(".searchsubmit-container").on("click", stopSearchIfEmpty);
 
-      jQuery(document).on('keydown', (e) => {
-          if (e.key === 'Escape') closeSearch();
-          if (e.key === 'Enter') stopSearchIfEmpty(e);
-        });
+    jQuery(document).on("keydown", (e) => {
+      if (e.key === "Escape") closeSearch();
+      if (e.key === "Enter") stopSearchIfEmpty(e);
     });
+  });
 };
