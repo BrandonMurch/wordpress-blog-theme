9a98306e197ff91072c91a4aadfeba0532ac9756
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2022-01-02 08:33:47 -0500

    Add avatar to article
    Reformat article metadata

--- single.php
+++ single.php
@@ -1,17 +1,45 @@
 <?php get_header(); ?>
 
+<?php
+$author_id = get_post_field("post_author");
+$author_name =
+    get_the_author_meta("first_name", $author_id) .
+    " " .
+    get_the_author_meta("last_name", $author_id);
+?>
+
 <main class="container">
 
-  <?php
-    if (has_post_thumbnail()) {
-        the_post_thumbnail("large");
-    }
-  ?>
+  <?php if (has_post_thumbnail()) {
+      the_post_thumbnail("large");
+  } ?>
 
-  <h1 class="display-3"><?php the_title(); ?></h1>
+  <h1 class="display-3 article-title"><?php the_title(); ?></h1>
 
-  <!-- <?php the_modified_date(); ?> -->
+  <div class="row article-meta-container">
+    <div class="col article-meta">
+      <img class="article-avatar" src="<?php echo esc_url(
+          get_the_author_meta("custom-avatar", $author_id)
+      ); ?>" alt="<?php echo $author_name; ?>'s Avatar'">
 
+      <p class="text-muted">
+
+        Written by <br>
+         <a class="text-muted" href="<?php echo get_the_author_meta(
+             "url",
+             $author_id
+         ); ?>">
+          <strong> <?php echo $author_name; ?> </strong>
+        </a>
+      </p>
+    </div>
+
+    <div class="col article-meta">
+      <p class="text-muted">Last updated <br> <strong><?php the_modified_date(); ?></strong>
+      </p>
+    </div>
+
+  </div>
   <?php the_content(); ?>
 
   <?php get_template_part("socials"); ?>
