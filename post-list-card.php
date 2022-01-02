5612cf07ba2e23dcb99bd3bb5bb3e1aba0a42ada
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2022-01-02 09:40:20 -0500

    Modify text based on update status

--- post-list-card.php
+++ post-list-card.php
@@ -1,21 +1,24 @@
 <article class="card">
-  <a href="<?php the_permalink() ?>" class="card-body">
-      <?php
-        if (has_post_thumbnail()) {
-            the_post_thumbnail("thumbnail");
-        }
-      ?>
+  <?php $has_been_modifed =
+      get_the_time(get_option("date_format")) != get_the_modified_date(); ?>
+
+  <a href="<?php the_permalink(); ?>" class="card-body">
+      <?php if (has_post_thumbnail()) {
+          the_post_thumbnail("thumbnail");
+      } ?>
       <h1 class="card-title">
       <?php the_title(); ?>
       </h1>
       <div class="row">
         <div class="col text-center">
-          <p class="text-muted">Written by <br> <strong> <?php the_author()?> </strong></p>
+          <p class="text-muted">Written by <br> <strong> <?php the_author(); ?> </strong></p>
           <p>
         </div>
 
         <div class="col text-center">
-          <p class="text-muted">Last updated <br> <strong><?php the_modified_date(); ?></strong>
+          <p class="text-muted"> <?php echo $has_been_modifed
+              ? "Last updated"
+              : "Created on"; ?> <br> <strong><?php the_modified_date(); ?></strong>
           </p>
         </div>
 
