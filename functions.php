a63cd0c0d4fcafe358b7c7e7432bf57687abc3fa
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2022-01-03 08:29:16 -0500

    Fix post card images

--- functions.php
+++ functions.php
@@ -66,6 +66,9 @@ add_theme_support("post-thumbnails");
 if (function_exists("add_image_size")) {
     add_image_size("avatar", $width = 48, $height = 48, $crop = false);
     add_image_size("avatar-large", $width = 96, $height = 96, $crop = false);
+    add_image_size("medium", $width = 400, $height = 400, $crop = false);
+    add_image_size("large", $width = 800, $height = 800, $crop = false);
+    add_image_size("avatar-large", $width = 96, $height = 96, $crop = false);
 }
 
 add_filter("image_size_names_choose", function ($sizes) {
@@ -78,3 +81,10 @@ add_filter("image_size_names_choose", function ($sizes) {
 add_filter("excerpt_length", function () {
     return 20;
 });
+
+function remove_img_attr($html)
+{
+    return preg_replace('/(width|height|sizes)="\d+"\s/', "", $html);
+}
+
+add_filter("post_thumbnail_html", "remove_img_attr");
