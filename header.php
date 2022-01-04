1be98effb4e79889bcd095330a8e4482e58eea73
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2022-01-03 22:10:36 -0500

    Transition header to flexbox

--- header.php
+++ header.php
@@ -14,19 +14,23 @@
 <body>
 
 <header class="header">
-    <svg id="svg" viewBox="0 220 1440 100" xmlns="http://www.w3.org/2000/svg">
+    <svg class="header-border" viewBox="0 220 1440 100" xmlns="http://www.w3.org/2000/svg">
       <path d="M 0,500 C 0,500 0,250 0,250 C 189.86666666666667,259.6 379.73333333333335,269.2 554,253 C 728.2666666666667,236.8 886.9333333333334,194.80000000000004 1032,190 C 1177.0666666666666,185.19999999999996 1308.5333333333333,217.59999999999997 1440,250 C 1440,250 1440,500 1440,500 Z" stroke="none" stroke-width="0"  transform="rotate(-180 720 250)">
       </path>
     </svg>
-  </div>
     <div class="logo-container">
       <a class="logo" href="<?php echo site_url(); ?>">
         bm
       </a>
     </div>
+
     <?php wp_nav_menu([
         "theme_location" => "header",
         "container_class" => "header-menu",
     ]); ?>
-    <?php get_search_form(); ?>
+
+    <div class="search-container">
+      <?php get_search_form(); ?>
+    </div>
+
 </header>
--- searchform.php
+++ searchform.php
@@ -3,16 +3,16 @@
 ); ?>">
 
   <label class="screen-reader-text" for="s">Search for:</label>
-  <input type="text" name="s" id="s" value="">
+  <input type="text" name="s" id="s" class="search-text" value="">
   <button type="button" name="button" role="presentation" class="search-toggle">
-    <?php get_template_part( "search-icon" ) ?>
+    <?php get_template_part("search-icon"); ?>
   </button>
   <button type="button" name="button" role="presentation" class="search-close">
-    <?php get_template_part( "close-icon" ) ?>
+    <?php get_template_part("close-icon"); ?>
   </button>
   <label class="searchsubmit-container">
     <input type="submit" id="searchsubmit">
-    <?php get_template_part( "search-icon" ) ?>
+    <?php get_template_part("search-icon"); ?>
   </label>
 
 </form>
