8ca927abf4625e1c8ab4ab18344fb64356b2848a
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2022-01-11 09:00:40 -0500

    Add default page

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<?php
-//  Silence is golden.
+<?php get_header(); ?>
+
+<main class="container">
+
+  <?php if (have_posts()):
+  	while (have_posts()):
+  		the_post();
+  		get_template_part("partials/templates/post-list-card");
+  	endwhile;
+  	get_template_part("partials/templates/post-list-navigation");
+  endif; ?>
+
+</main>
+
+
+<?php get_footer(); ?>
