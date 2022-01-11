177bee1f7c68b2143130992e6219be31ec291824
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2022-01-11 09:02:30 -0500

    Fix search slide on chrome

--- js/search.js
+++ js/search.js
@@ -8,6 +8,11 @@ export const searchBarHandler = () => {
 		jQuery("#searchform")[0].reset();
 	});
 
+	// Javascript work-around for chrome-based browsers since they don't seem to allow focus-within a form using a button. Have tried placing tabIndex on all interior elements, no change.
+	jQuery(".searchform").on("click", () => {
+		jQuery("#s").focus();
+	});
+
 	jQuery(document).on("keydown", (e) => {
 		if (e.key === "Escape") document.activeElement.blur();
 	});
